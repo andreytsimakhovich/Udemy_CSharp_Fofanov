61be5ca3f7d2d2bd624c33dabb11498dce410e58
Andrei Tsimakhovich <taa421992@gmail.com>
2021-02-12 12:08:56 +0300

    Creating a repository. Adding files associated with the project. Adding a Readme file.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Udemy_CSharp_Fofanov
---
+++ Udemy_CSharp_Fofanov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_CSharp_Fofanov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
