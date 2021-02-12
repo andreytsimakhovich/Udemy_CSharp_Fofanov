29eefb0bb08998c3ad6c466c33f88e483eb912a4
Andrei Tsimakhovich <taa421992@gmail.com>
2021-02-12 13:44:18 +0300

    Add 12_Lesson.

---
+++ 12_Lesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -1;
+
+            int y;
+
+            y = 2;
+
+            var a = 1; /* есть сложные типы которые имеют длинную запись, в этом случае 
+                        * применяют var 
+                        */
+            string name = "John";
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+
+            // переменные нужно именовать значимо!!!
+
+        }
+    }
+}
---
+++ 13_Lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
