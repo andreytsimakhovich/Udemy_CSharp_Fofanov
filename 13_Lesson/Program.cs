9915d9ce9ae3b29bca90dc08118eec6a518de9a5
Andrei Tsimakhovich <taa421992@gmail.com>
2021-02-22 14:02:10 +0300

    Add 16_Lesson.

--- 13_Lesson/Program.cs
+++ 13_Lesson/Program.cs
@@ -6,7 +6,62 @@ namespace _13_Lesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 2;
+            int b = 5;
+
+            bool argsEqual = a == b;
+            Console.WriteLine(argsEqual);
+        }
+
+        static void MathOperations()
+        {
+            //Взятие остатка от числа
+
+            int x;
+            x = 4 % 2;
+            Console.WriteLine(x);
+            x = 5 % 2;
+            Console.WriteLine(x);
+
+            // Умножение
+
+            int a;
+            a = 2 * 2;
+            Console.WriteLine(a);
+
+            a *= 2;
+            Console.WriteLine(a);
+        }
+        static void IncrementDecrement()
+        {
+            // Самое простое и доходчивое объяснение инкремента в постфиксном и префискном представлении
+            int x = 0;
+            int y = x++;
+            Console.WriteLine($"Значение Y: {y}");
+            Console.WriteLine($"Значение X: {x}\n");
+
+            y = ++x;
+            Console.WriteLine($"Значение Y: {y}");
+            Console.WriteLine($"Значение X: {x}\n");
+
+            x += 2;
+            // x = x + 2;
+            Console.WriteLine($"x + 2 = {x}");
+            x -= 3;
+            // x = x - 3;
+            Console.WriteLine($"x - 3 = {x}");
+
+            x = 2;
+            y = x--;
+            Console.WriteLine($"Значение Y: {y}");
+            Console.WriteLine($"Значение X: {x}\n");
+
+            y = --x;
+            Console.WriteLine($"Значение Y: {y}");
+            Console.WriteLine($"Значение X: {x}\n");
+
+
+
         }
     }
 }
