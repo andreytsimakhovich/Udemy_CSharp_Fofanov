033a2bb3ee45bf505af931b12d38b113a2d8488a
Andrei Tsimakhovich <taa421992@gmail.com>
2021-02-23 14:47:54 +0300

    Update Lesson_19 and ReadMe.

---
+++ 19_Lesson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _19_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string emphy = "";
+            //string whiteSpased = " ";
+            //string notEmphy = " b";
+            //string nullStr = null;
+
+            //Console.WriteLine("IsNullOrEmphy");
+            //bool isNullOrEmphy = string.IsNullOrEmpty(emphy);
+            //Console.WriteLine(isNullOrEmphy);
+
+            //isNullOrEmphy = string.IsNullOrEmpty(whiteSpased);
+            //Console.WriteLine(isNullOrEmphy);
+
+            //isNullOrEmphy = string.IsNullOrEmpty(notEmphy);
+            //Console.WriteLine(isNullOrEmphy);
+
+            //isNullOrEmphy = string.IsNullOrEmpty(nullStr);
+            //Console.WriteLine(isNullOrEmphy);
+
+            //Console.WriteLine("\nIsNullOrWhiteSpased");
+            //bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(whiteSpased);
+            //Console.WriteLine(isNullOrWhiteSpace);
+
+            //isNullOrWhiteSpace = string.IsNullOrWhiteSpace(nullStr);
+            //Console.WriteLine(isNullOrWhiteSpace);
+
+
+            string a = "";
+
+            bool result = string.IsNullOrEmpty(a);
+            Console.WriteLine(result);
+
+            result = string.IsNullOrWhiteSpace(a);
+            Console.WriteLine(result);
+        }
+    }
+}
