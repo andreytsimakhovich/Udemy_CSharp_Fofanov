b20bd973f55fbaa07d16261bace3d34e6a19a310
Andrei Tsimakhovich <taa421992@gmail.com>
2021-02-24 14:18:23 +0300

    Add 21_Lesson.

---
+++ 21_Lesson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _21_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Хорошо использовать для сложения большого количества строк
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My ");
+            sb.Append("name ");
+            sb.Append("is ");
+            sb.Append("Andrey!");
+            sb.Append("!");
+            sb.Append("Hello!");
+
+            string str = sb.ToString();
+            Console.WriteLine(str);
+        }
+    }
+}
