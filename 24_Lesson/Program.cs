8833f783f0505281430f3bca34530c1d07d27cec
Andrei Tsimakhovich <taa421992@gmail.com>
2021-03-02 13:40:34 +0300

    Add 24_Lesson

---
+++ 24_Lesson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _24_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, please tell me your name");
+
+            string name = Console.ReadLine();
+            string sentence = $"Your name is {name}";
+            Console.WriteLine(sentence);
+
+            Console.WriteLine("Please, tell me your age");
+            
+            string input = Console.ReadLine();
+            int age = int.Parse(input);
+            Console.WriteLine($"Your age is {age}");
+
+            Console.BackgroundColor = ConsoleColor.Cyan;
+        }
+    }
+}
