56099237c43ce9bc5086f996fe83bbd7942e2b9f
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-12 16:11:25 +0300

    Add 29_Lesson

---
+++ 29_Lesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine(now.ToString());
+
+            DateTime dt = new DateTime(2016, 2, 28);
+            Console.WriteLine(dt);
+
+            DateTime newDt = dt.AddDays(2);
+            Console.WriteLine(newDt);
+
+            TimeSpan ts = now - dt;
+            Console.WriteLine(ts.Days);
+        }
+    }
+}
