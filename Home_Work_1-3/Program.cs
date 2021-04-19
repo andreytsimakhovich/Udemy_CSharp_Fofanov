90f82345ce28f6edb78a26482f5fc7c2ad7c2216
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-19 16:28:04 +0300

    Add Home_Work_1-3. 1. Запросить имя пользователя. Вывести Hello, [имя пользователя].
    2. Запросить у пользователя два целых числа и сохранить в двух переменных. Вывести значения.
    Обменять значения переменных: например, если в переменной x было записано число 3, а в y число 5, сделать так, чтобы в y стало 3, а в x стало 5. Вывести значения после обмена.
    
    3. Запросить у пользователя целое число. Вывести количество цифр числа. Например, в числе 156 - 3 цифры.

---
+++ Home_Work_1-3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_1_3
+{
+    class Program
+    {
+
+        static int Foo(int value)
+        {
+            if (value < 10)
+                return value;
+
+            int digit = value % 10;
+
+            int nextValue = value / 10;
+
+            return digit + Foo(nextValue);
+        }
+        static void Main(string[] args)
+        {
+            //1
+            //Console.WriteLine("Введите имя:");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Ваше имя:" + name);
+            //Console.ReadKey();
+
+            //2
+            //Console.WriteLine("Введите два целых числа:");
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Число 1:    {x}\nЧисло 2:    {y} ");
+            //Console.WriteLine("Замена чисел:");
+            //Console.WriteLine($"Число 1:    {y}\nЧисло 2:    {x} ");
+
+            //3
+            int myValue = 156;
+
+            int result = Foo(myValue);
+
+            Console.WriteLine(result);
+        }
+    }
+}
