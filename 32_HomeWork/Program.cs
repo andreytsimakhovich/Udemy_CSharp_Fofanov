74c69b3e469b653b9ff2cc9198540d54f9efa31e
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-19 16:55:22 +0300

    Add 32_HomeWork.
    Запросить у пользователя длины трёх сторон треугольника. Длины могут быть представлены дробными значениями.
    
    После получения длин сторон - использовать формулу Герона для вычисления площади треугольника.
    
    Чтобы жизнь не казалась мёдом найдите формулу самостоятельно.
    
    После вычисления площади - вывести результат на консоль.

---
+++ 32_HomeWork/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение стороны треугольника №1:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение стороны треугольника №2:");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение стороны треугольника №3:");
+            double c = double.Parse(Console.ReadLine());
+
+            double p = (a + b + c) / 2;
+
+            double S = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
+            
+            Console.WriteLine($"Площадь треугольника: {Math.Round(S, 2)}");
+            Console.ReadKey();
+        }
+    }
+}
