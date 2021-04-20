20449c3be0011038a968957f824f25165696a096
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-20 16:46:45 +0300

    Add 49_HomeWork.
    Факториалом числа является произведение этого числа на все предшествующие этому числу числа (кроме 0). Факториал в математике записывается через восклицательный знак. Например 5! = 5*4*3*2*1 = 120. Особые случаи: 0! = 1. 1! = 1.
    
    Задача: запросить у пользователя число, факториал которого необходимо вычислить и произвести вычисление. Затем вывести результат вычисления. Восклицательный знак запрашивать не надо, кроме того, в C# такой операции нет. Для вычисления факториала надо производить перемножение.

---
+++ 49_HomeWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to calculate the factorial:");
+            int number = int.Parse(Console.ReadLine());
+
+            long factorial = 1;
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("Calculation result: " + factorial);
+
+            Console.ReadKey();
+        }
+    }
+}
