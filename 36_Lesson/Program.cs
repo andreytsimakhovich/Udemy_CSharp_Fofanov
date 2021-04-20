1ba2dfc4fe10e25b95c5f111bd49ae7bfc9a7f50
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-20 10:15:02 +0300

    Add 37_HomeWork. Запросить у пользователя два целочисленных значения и найти максимальное.

---
+++ 36_Lesson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your weight:");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height:");
+            double height = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            double bodyMassIndex = weight / weight * height;
+
+            bool isTooLow = bodyMassIndex <= 18.5;
+            bool isNormal = bodyMassIndex >18.5 && bodyMassIndex < 25;
+            bool isAboveNormal = bodyMassIndex >= 25 && bodyMassIndex <= 30;
+            bool isTooFat = bodyMassIndex >  30;
+
+            bool isFat = isAboveNormal || isTooFat;
+            
+            Console.WriteLine(bodyMassIndex);
+
+            if (isFat)
+            {
+                Console.WriteLine("You'd better lose some weight");
+            }
+
+            else
+            {
+                Console.WriteLine("You're in a good shape");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 37_HomeWork/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число b:");
+            int b = int.Parse(Console.ReadLine());
+
+            //if (a > b)
+            //    Console.WriteLine($"Число {a} больше числа {b} ");
+            //else if (b > a)
+            //    Console.WriteLine($"Число {b} больше числа {a} ");
+            //else if (a == b)
+            //    Console.WriteLine($"Числа {a} = {b} ");
+            //else
+            //    Console.WriteLine("Неверное значение!");
+            //Console.ReadKey();
+
+
+            int maxValue = a > b ? a : b;
+            Console.WriteLine($"Максимальное значение: { maxValue}");
+            Console.ReadKey();
+        }
+    }
+}
