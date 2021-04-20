e5192a3d6cd87b8a083426936720b6e7916cbd88
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-20 11:23:21 +0300

    Add 40_Lesson.

---
+++ 40_Lesson/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { -1, 1, 2, 3, 7, -7, -2, 9, -4, -8, 0, 4, 9, 7, 9, 4 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1 ; j < numbers.Length; j++)
+                {
+                    int atI = numbers[i];
+                    int atJ = numbers[j];
+
+                    if (atI + atJ == 0)
+                    {
+                        Console.WriteLine($"Пары: ({atI};{atJ}). Indexes({i};{j})");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
