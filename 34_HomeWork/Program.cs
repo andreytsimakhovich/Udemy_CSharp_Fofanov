14d8eb1c1677ca80f106c2d2206be98c84996a0a
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-20 08:28:25 +0300

    Add 34_HomeWork.
    Запросить у пользователя: фамилию, имя, возраст, вес, рост.
    
    Высчитать ИМТ (индекс массы тела) по формуле ИМТ = вес (кг) / (рост (м) * рост (м))
    
    Сформировать единую строку, в следующем формате:
    
    Your profile:
    Full Name: фамилия, имя
    Age: возраст
    Weight: вес
    Height: рост
    Body Mass Index: ИМТ
    
    Вывести сформированную строку на консоль.

---
+++ 34_HomeWork/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34_HomeWork
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your firstname:");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter your lastname:");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your weight:");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height:");
+            double height = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            double bodyMassIndex = weight / height * height;
+
+            string profile =
+                  $"Your profile: {Environment.NewLine}"
+                + $"Full Name: {lastname} {firstname} {Environment.NewLine}"
+                + $"Age: {age}{Environment.NewLine}"
+                + $"Weight: {weight}{Environment.NewLine}"
+                + $"Height: {height}{Environment.NewLine}"
+                + $"Body Mass Index: {bodyMassIndex}{Environment.NewLine}";
+
+            Console.WriteLine(profile);
+            Console.ReadKey();
+        }
+    }
+}
