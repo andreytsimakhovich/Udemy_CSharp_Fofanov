9324ad82206edefc4cde07bb23951a9c4216c034
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-21 10:32:26 +0300

    Add 51_HomeWork.
    Предположим, что логин\пароль для входа в систему: johnsilver\qwerty.
    
    Запросить у пользователя логин и пароль.
    Дать пользователю только три попытки для ввода корректной пары логин\пароль.
    Если пользователь произвёл корректный ввод, вывести на консоль:
    "Enter the System" и прекратить запрос логина\пароля.
    Если пользователь ошибся трижды - вывести "The number of available tries have been exceeded" и прекратить запрос пары логин\пароль.

---
+++ 51_HomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (true)
+            {
+                Console.WriteLine("Enter your login:");
+                string login = Console.ReadLine();
+
+                Console.WriteLine("Enter your password:");
+                string password = Console.ReadLine();
+
+                    if (login == "andrey" && password == "1234")
+                    {
+                        Console.WriteLine("Enter the system!");
+                        break;
+                    }
+                    else
+                    {
+                        counter++;
+
+                        if (counter < 3)
+                            Console.WriteLine("The username or password is incorrect! Try again.");
+                        else
+                        {
+                            Console.WriteLine("The number of available tries have been exceeded!");
+                            break;
+                        }
+                    }
+            }
+            Console.ReadLine();
+        }
+    }
+}
