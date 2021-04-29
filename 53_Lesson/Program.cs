41216a903d07ee92eb36b219ce9d01fd96f92fff
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-04-29 14:28:24 +0300

    Add 53_Lesson.

---
+++ 53_Lesson/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (var item in array1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nCopy:");
+
+            int[] array2 = new int[10];
+            array1.CopyTo(array2, 0);   // копирование массива array1 to array2
+            foreach (var item in array2)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nRevers:");
+
+            Array.Reverse(array1);
+            foreach (var item in array1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nSort:");
+
+            Array.Sort(array1);
+            foreach (var item in array1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nClear:");
+
+            Array.Clear(array1, 0, array1.Length);
+            foreach (var item in array1)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
