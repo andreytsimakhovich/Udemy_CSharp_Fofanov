2f6ebbb7e6a3a208e0141c4c12be491d91fa6700
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-05-04 13:17:40 +0300

    Add 65_Lesson.

---
+++ 65_Lesson/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _65_Lesson
+{
+    public class Calculator
+    {
+        public double triangleArea (double a, double b)
+        {
+           return 0.5 * a * b;
+        }
+
+        public double triangleArea(double a)
+        {
+            return Math.Round((Math.Sqrt(3) / 4) * Math.Pow(a, 2), 2);
+        }
+
+        public double triangleArea(double a, double b, double c)
+        {
+            double p = 0.5 * (a + b + c);
+            return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)), 2);
+        }
+    }
+}
---
+++ 65_Lesson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _65_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine("Enter values for triangle: ");
+            double S = calc.triangleArea(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Area of the triagle = " + S);
+            Console.ReadKey();
+        }
+    }
+}
