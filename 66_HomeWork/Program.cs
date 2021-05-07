998781e7d2d7ebdb24ca49cf9e3eced69c7f6a72
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-05-07 12:19:39 +0300

    Add 66_Homework.

---
+++ 66_HomeWork/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RomanNumeral.RomanToInteger("XIV"));
+            Console.ReadKey();
+        }
+    }
+}
