f5efcd6ff98495fd9ae0a990e1345d8dfa7f10e8
Tsimakhovich_Andrei <taa421992@gmail.com>
2021-05-07 14:05:48 +0300

    Add 70_Lesson

---
+++ 70_Lesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70_Lesson
+{
+    class Program
+    {
+        static void Array(string str, params int[] numbers)
+        {
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(str);
+        }
+
+        static void Main(string[] args)
+        {
+            Array("hello", 1, 2, 3, 4, 5);
+            Console.ReadLine();
+        }
+    }
+}
